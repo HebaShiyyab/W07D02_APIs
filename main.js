cb31b2bf0f5fcfa758150af3bc058816c277262b
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-05-10 23:54:58 +0300

    setup server

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 4000;
+app.get("/heba", (req,res)=>{
+    res.status(200);
+    res.json("welcome")
+})
+
+
+
+ app.listen(port,()=>{
+     console.log(`server run on port ${port}`);
+ })
